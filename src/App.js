8fd652a5c69f642e4bdb5d5d91aa9ca23d4e61e9
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-09-13 12:37:17 +0300

    first task for work aswar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="card">
+        <div className="card-image">	
+          <h2 className="card-heading">
+            Get started
+            <small>Let us your Rate for stuff</small>
+          </h2>
+        </div>
+        <form className="card-form">
+          <div className="input">
+            <input type="text" className="input-field" required/>
+            <label className="input-label">Stuff name</label>
+          </div>
+          <div className="input">
+            <input type="number" className="input-field" required/>
+            <label className="input-label">Quantity</label>
+          </div>
+          <div className="input">
+            <select className="input-field" style={{color: "#8597a3"}} required>
+              <option style={{color: "black"}} value="/">Condition</option>
+              <option style={{color: "black"}} value="excellent">excellent</option>
+              <option style={{color: "black"}} value="good">good</option>
+              <option style={{color: "black"}} value="fair">fair</option>
+              <option style={{color: "black"}} value="poor">poor</option>
+            </select>
+          </div>
+          <div className="action">
+            <button className="action-button">Get started</button>
+          </div>
+        </form>
+	</div>
   );
 }
 
