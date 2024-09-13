cbb47d7a8b2968c31580e3d13a5c313caeac03b5
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-09-13 23:38:06 +0300

    add from the form and show in the table

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,59 +1,72 @@
 import { useState } from 'react';
-import Table from './Table';
+import { Link } from 'react-router-dom';
 
 function Form() {
-    const [stuffName, setStuffName] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [condition, setCondition] = useState('');
+    const [stuff, setStuff] = useState({
+        stuffName: '',
+        quantity: '',
+        condition: '',
+    });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(stuffName);
-        console.log(quantity);
-        console.log(condition);
-        setStuffName(prev => [...prev, stuffName])   
-        setQuantity(prev => [...prev, quantity])   
-        setCondition(prev => [...prev, condition])   
-    }
+    const change = (e) => {
+        setStuff({ ...stuff, [e.target.name]: e.target.value });
+        localStorage.setItem('stuffs', stuff.stuffName)
+    };
 
     return (
         <>
             <div className="card">
                 <div className="card-image">	
-                <h2 className="card-heading">
-                    Get started
-                    <small>Let us your Rate for stuff</small>
-                </h2>
+                    <h2 className="card-heading">
+                        Get started
+                        <small>Let us your Rate for stuff</small>
+                    </h2>
                 </div>
-            <form className="card-form" onSubmit={handleSubmit}>
-                <div className="input">
-                    <input type="text" value={stuffName} 
-                    onChange={(e) => setStuffName(e.target.value)} 
-                    className="input-field" required/>
-                    <label className="input-label">Stuff name</label>
-                </div>
-                <div className="input">
-                    <input type="number" value={quantity} 
-                    onChange={(e) => setQuantity(e.target.value)}
-                    className="input-field" required/>
-                    <label className="input-label">Quantity</label>
-                </div>
-                <div className="input">
-                    <select className="input-field" value={condition}
-                    onChange={(e) => setCondition(e.target.value)} required>
-                    <option value="/">Condition</option>
-                    <option value="excellent">excellent</option>
-                    <option value="good">good</option>
-                    <option value="fair">fair</option>
-                    <option value="poor">poor</option>
-                    </select>
-                </div>
-                <div className="action">
-                    <button className="action-button">Get started</button>
-                </div>
-            </form>
+                <form className="card-form">
+                    <div className="input">
+                        <input 
+                            type="text" 
+                            name="stuffName"
+                            value={stuff.stuffName} 
+                            onChange={change}
+                            className="input-field" 
+                            required
+                        />
+                        <label className="input-label">Stuff name</label>
+                    </div>
+                    <div className="input">
+                        <input 
+                            type="number" 
+                            name="quantity" 
+                            value={stuff.quantity} 
+                            onChange={change}
+                            className="input-field" 
+                            required
+                        />
+                        <label className="input-label">Quantity</label>
+                    </div>
+                    <div className="input">
+                        <select 
+                            name="condition"
+                            value={stuff.condition} 
+                            onChange={change} 
+                            className="input-field" 
+                            required
+                        >
+                            <option value="">Condition</option> 
+                            <option value="excellent">excellent</option>
+                            <option value="good">good</option>
+                            <option value="fair">fair</option>
+                            <option value="poor">poor</option>
+                        </select>
+                    </div>
+                    <div className="action">
+                        <Link to="/Table" state={stuff} className='link'>
+                            <button className="action-button">Get started</button>
+                        </Link>
+                    </div>
+                </form>
             </div>
-            <Table/>
         </>
     );
 }
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import stuff from './Form'
 
-const Table = (props) => {
+const Table = () => {
+    const Location = useLocation();
+    const [state, setLocationState] = useState({stuff});
+
+    useEffect(() => {
+        let state = Location.state
+        setLocationState(state)
+    }, [Location.state])
+    
+    const navigate = useNavigate();
+    const handleRowClick = () => {
+        navigate('/form');
+    }
+
     return (
         <div>
             <table>
@@ -9,11 +24,13 @@ const Table = (props) => {
                     <th>Lastname</th>
                     <th>Savings</th>
                 </tr>
-                <tr>
-                    <td>{props.stuffName}</td>
-                    <td>{props.quantity}</td>
-                    <td>{props.condition}</td>
+                {state && (
+                <tr onClick={handleRowClick}>
+                    <td>{state.stuffName}</td>
+                    <td>{state.quantity}</td>
+                    <td>{state.condition}</td>
                 </tr>
+                )}
             </table>
         </div>
     );
