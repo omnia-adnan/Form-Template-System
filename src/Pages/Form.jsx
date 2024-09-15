ca91d4c5832aaf47d0b958ee16cf3b2cc0637d6c
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-09-15 14:58:44 +0300

    styling template form

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,79 +1,37 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './Form.css';
 
 function Form() {
-    const [stuff, setStuff] = useState({
-        stuffName: '',
-        quantity: '',
-        condition: '',
-    });
-
-    const change = (e) => {
-        const { name, value } = e.target;
-        setStuff((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
-
-    const handleSaveData = () => {
-        localStorage.setItem('stuffs', JSON.stringify(stuff));
-    };
+    const [formContent, setFormContent] = useState([]);
+    const addQuestion = () => {
+        const field ={
+            "name": `question_${formContent.length}`,
+            "lable": "Untitled question",
+            "question_type": "showt question", // "paregraph", "multichoice",
+            "list": [],
+        }
+        setFormContent([...formContent, field]);
+    }
 
     return (
-        <div className='contenar'>
-            <div className="card">
-                <div className="card-image">	
-                    <h2 className="card-heading">
-                        Get started
-                        <small>Let us know your rate for stuff</small>
-                    </h2>
+        <div className='container'>
+            <div className='title'>
+                <h1>Form Mark</h1>
+                <h2>Untitled Form</h2>
+            </div>
+            <div className='questions'>
+                {formContent.map((field, index) => {
+                    return(
+                        <div key={index}>
+                            test
+                        </div>
+                    )
+                })}
+                <div className='button'>
+                    <div>
+                        <button type="button" onClick={addQuestion}>Add Question</button>
+                    </div>
                 </div>
-                <form className="card-form">
-                    <div className="input">
-                        <input 
-                            type="text" 
-                            name="stuffName"
-                            value={stuff.stuffName} 
-                            onChange={change}
-                            className="input-field" 
-                            required
-                        />
-                        <label className="input-label">Stuff name</label>
-                    </div>
-                    <div className="input">
-                        <input 
-                            type="number" 
-                            name="quantity" 
-                            value={stuff.quantity} 
-                            onChange={change}
-                            className="input-field" 
-                            required
-                        />
-                        <label className="input-label">Quantity</label>
-                    </div>
-                    <div className="input">
-                        <select 
-                            name="condition"
-                            value={stuff.condition} 
-                            onChange={change} 
-                            className="input-field" 
-                            required
-                        >
-                            <option value="">Condition</option> 
-                            <option value="excellent">Excellent</option>
-                            <option value="good">Good</option>
-                            <option value="fair">Fair</option>
-                            <option value="poor">Poor</option>
-                        </select>
-                    </div>
-                    <div className="action" onClick={handleSaveData}>
-                        <Link to="/Table" state={stuff} className='link'>
-                            <button className="action-button">Get started</button>
-                        </Link>
-                    </div>
-                </form>
             </div>
         </div>
     );
