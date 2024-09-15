0a66dafb1ea1b25f2268f1d29df54938ab3c6fb1
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-09-15 16:30:44 +0300

    function choicen type

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import './Form.css';
 
+
 function Form() {
     const [formContent, setFormContent] = useState([]);
     const addQuestion = () => {
         const field ={
             "name": `question_${formContent.length}`,
-            "lable": "Untitled question",
-            "question_type": "showt question", // "paregraph", "multichoice",
+            "label": "Untitled question",
+            "question_type": "showt_answer", // "paregraph", "multichoice",
             "list": [],
         }
         setFormContent([...formContent, field]);
@@ -20,10 +21,35 @@ function Form() {
                 <h2>Untitled Form</h2>
             </div>
             <div className='questions'>
-                {formContent.map((field, index) => {
+                {formContent.map((field) => {
                     return(
-                        <div key={index}>
-                            test
+                        <div className='question_type'>
+                            <div className='question_choice'>
+                                <div key={field.name}>
+                                    <label>{field.label}</label>
+                                </div>
+                                <div className='choices'>
+                                    <select>
+                                        <option value="showt_answer">Showt Answer</option>
+                                        <option value="paregraph">Paregraph</option>
+                                        <option value="multichoice">Multichoice</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className='choice_type'>
+                                {
+                                    field.question_type === 'showt_answer' && <input type="text" placeholder={field.label} />
+                                }
+                                {
+                                    field.question_type === 'paregraph' && <textarea rows={4} placeholder={field.label} />
+                                }
+                                {
+                                    field.question_type === 'multichoice' && 
+                                        <select>
+                                            {field.list.map((item) => <option key={item} value={item}>{item}</option>)}
+                                        </select>
+                                }
+                            </div>
                         </div>
                     )
                 })}
@@ -33,6 +59,7 @@ function Form() {
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
